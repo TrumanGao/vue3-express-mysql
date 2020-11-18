004efe2019842e28ca97561e89439c6decb1e7b8
gaofeixiang <gaofeixiang@fenbi.com>
2020-11-18 18:50:46 +0800

    初始化

---
+++ web/src/router/index.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () =>
+        import(
+          /* webpackChunkName: "view-home" */ "@/views/Home.vue"
+        ),
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "view-about" */ '@/views/About.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
